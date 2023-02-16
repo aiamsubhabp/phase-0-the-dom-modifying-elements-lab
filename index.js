038e770d1fbdc75b11bc2106f813886f7e38c520
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-02-16 00:51:46 -0500

    Complete Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const element = document.createElement('main');
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Shrek is the champion"
+
+
+
